487c6a6e43c709a8755dddbbc6eda01967e3f46e
nameapp <yashma.sonara04@gmail.com>
2023-01-01 20:46:03 +0800

    added association between post and user

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_01_120508) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_01_121404) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
