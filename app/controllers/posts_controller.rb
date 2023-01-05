d433a5452623aa46690a99f62cd7eae175bf4f31
nameapp <yashma.sonara04@gmail.com>
2023-01-06 02:14:25 +0800

    still have issue with delete function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,53 +1,51 @@
 class PostsController < ApplicationController
-    before_action :find_post, only: [:show, :edit, :update, :destroy]
-    before_action :authenticate_user!, except: [:index, :show]
-    def index
-        @posts = Post.all.order("created_at DESC")
-    end
+	before_action :find_post, only: [:show, :edit, :update, :destroy]
+	before_action :authenticate_user!, except: [:index, :show]
 
-    def new 
-        @post = current_user.posts.build
-    end
+	def index
+		@posts = Post.all.order("created_at DESC")
+	end
 
-    def create 
-        @post = current_user.posts.build(post_params)
+	def show
+	end
 
-        if  @post.save 
-            redirect_to @post 
+	def new
+		@post = current_user.posts.build
+	end
 
-        else 
-            render 'new' 
-        end
-    end
+	def create
+		@post = current_user.posts.build(post_params)
 
-    def edit 
-    end 
+		if @post.save
+			redirect_to @post
+		else
+			render 'new'
+		end
+	end
 
-    def update 
-        if @post.update(post_params)
-            redirect_to @post
-           else
-            render 'edit'
-           end
-    end 
+	def edit
+	end
 
-    def destroy 
-        @post.destroy
-        redirect_to root_path
-    end 
+	def update
+		if @post.update(post_params)
+			redirect_to @post
+		else
+			render 'edit'
+		end
+	end
 
-    def show 
-       
-    end 
+	def destroy
+		@post.destroy
+		redirect_to root_path
+	end
 
-    private
+	private
 
-    def post_params 
-        params.require(:post).permit(:title, :content)
-    end 
+	def find_post
+		@post = Post.find(params[:id])
+	end
 
-    def find_post 
-        @post = Post.find(params[:id])
-    end
-
+	def post_params
+		params.require(:post).permit(:title, :content)
+	end
 end
