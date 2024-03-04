2110b5b661e837637cf3e700c7162b638d9cf577
yashma-sonara <yashma.sonara04@gmail.com>
2024-03-04 22:23:21 +0800

    Update navigation and css

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :posts do
-  resources :comments
-  
+    resources :comments
   end
-root 'posts#index'
-get 'tags/:tag', to: 'posts#index', as: :tag
+  root 'posts#index'
+  get 'tags/:tag', to: 'posts#index', as: :tag
+  delete 'posts/:id', to: 'posts#destroy', as: 'delete_post'
 end
